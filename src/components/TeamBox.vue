c374de3cd29c94874a4a1be6de8fd483195ceaa7
Kevin Castillo <kevinacastles@gmail.com>
2024-06-02 01:30:34 -0400

    style: change background colors

--- src/components/TeamBox.vue
+++ src/components/TeamBox.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="flex justify-center">
     <div class="w-fit flex flex-col justify-center items-center">
-      <div class="flex bg-slate-100 rounded-2xl pt-4 px-2 pb-1">
+      <div class="flex bg-red-300 rounded-2xl pt-4 px-2 pb-1">
         <div v-for="i in 6" :key="i" class="relative w-10 h-10 mx-2">
           <img
             class="absolute z-10 max-w-24 w-20 h-auto top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
@@ -9,7 +9,7 @@
             alt=""
           />
           <div
-            class="w-8 h-4 bg-slate-400 rounded-[50%] absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"
+            class="w-8 h-4 bg-slate-700 rounded-[50%] absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0"
           ></div>
         </div>
       </div>
