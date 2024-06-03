162dc5902c7342d9cbdf8c6b387687c4a51a0809
Kevin Castillo <kevinacastles@gmail.com>
2024-06-03 02:45:27 -0400

    feat: add more api routes and types

--- src/api/pokeApi.ts
+++ src/api/pokeApi.ts
@@ -1,21 +1,32 @@
 import axios from 'axios'
 import type {
+  EvolutionResponse,
   PokeApiResponse,
   PokedexResponse,
   Pokemon,
-  PokemonSummary
+  PokemonSummary,
+  SpeciesResponse
 } from '@/types/PokeApiResponse'
 
-const url = 'https://pokeapi.co/api/v2/pokemon/'
+const url = 'https://pokeapi.co/api/v2/'
 
 export default async function getPokeApiList(offset = 0, limit = 25): Promise<PokeApiResponse> {
-  const response = await axios.get<PokeApiResponse>(`${url}?offset=${offset}&limit=${limit}`)
-  return response.data
+  try {
+    const response = await axios.get<PokeApiResponse>(
+      `${url}pokemon/?offset=${offset}&limit=${limit}`
+    )
+    return response.data
+  } catch (error) {
+    console.error(`Failed to fetch data pokemon list`, error)
+    throw error
+  }
 }
 
 export async function getPokedexList(offset = 0, limit = 25): Promise<PokedexResponse> {
   try {
-    const response = await axios.get<PokeApiResponse>(`${url}?offset=${offset}&limit=${limit}`)
+    const response = await axios.get<PokeApiResponse>(
+      `${url}pokemon/?offset=${offset}&limit=${limit}`
+    )
 
     const results: PokemonSummary[] = await Promise.all(
       response.data.results.map(async (pokemon) => {
@@ -43,6 +54,39 @@ export async function getPokedexList(offset = 0, limit = 25): Promise<PokedexRes
     return pokedexResponse
   } catch (error) {
     console.error('Failed to fetch pokedex list', error)
+    throw error
+  }
+}
+
+export async function getPokemonById(id: number): Promise<Pokemon> {
+  try {
+    const response = await axios.get<Pokemon>(`${url}pokemon/${id}`)
+
+    return response.data
+  } catch (error) {
+    console.error(`Failed to fetch data for pokemon with id: ${id}`, error)
+    throw error
+  }
+}
+
+export async function getEvolutionChain(id: number): Promise<EvolutionResponse> {
+  try {
+    const response = await axios.get<EvolutionResponse>(`${url}evolution-chain/${id}`)
+
+    return response.data
+  } catch (error) {
+    console.error(`Failed to fetch data for evolution chain id: ${id}`, error)
+    throw error
+  }
+}
+
+export async function getPokemonSpecies(speciesUrl: string): Promise<SpeciesResponse> {
+  try {
+    const response = await axios.get<SpeciesResponse>(`${speciesUrl}`)
+
+    return response.data
+  } catch (error) {
+    console.error(`Failed to fetch data for pokemon species`, error)
     throw error
   }
 }
